366116b640e1ae434996d9cfcac1b2633b816c76
LAKSHMI NARAYANA REDDY P <109896830+2100030418@users.noreply.github.com>
2022-10-22 12:04:15 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-       <form className="form" onSubmit={this.handleSubmit}>
-          <div className="input-group">
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" placeholder="nome@email.com.br" />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">Senha</label>
-            <input type="password" name="password" />
-          </div>
-          <button className="primary">ENTRAR</button>
-        </form>
-        <button className="secondary" onClick={this.handleClick}>
-          Criar uma nova conta
-        </button>
+       <input type="email" name="email" placeholder="nome@email.com.br" />
+    <input type="password" name="password" />
+    <button className="primary">LOGIN</button>
+         
     </div>
   );
 }
